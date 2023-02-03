b695036097869ef60a8a1cdaa11bccd6de685e6a
Vilen Evseev <vievseev@ozon.ru>
2023-02-03 14:27:09 +0300

    init commit

---
+++ shared/src/commonMain/kotlin/dev/holdbetter/premierleague/Greeting.kt
@@ -0,0 +1,9 @@
+package dev.holdbetter.premierleague
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
