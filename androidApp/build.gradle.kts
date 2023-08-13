01d5cfa947ac9b49e4e8d0d7f0a12c173179d163
Vilen Evseev <holdbetter1@gmail.com>
2023-08-13 23:24:52 +0300

    version update

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.holdbetter.premierleague.android"
         minSdk = 26
         targetSdk = 32
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
     packagingOptions {
         resources {
